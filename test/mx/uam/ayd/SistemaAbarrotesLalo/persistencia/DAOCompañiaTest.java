ca49ccefc003465d935332c8f9ecf59aa1119dae
Byakuya <Byakuya@192.168.1.69>
2018-03-18 20:59:07 -0600

    4 Hu terminadas

---
+++ test/mx/uam/ayd/SistemaAbarrotesLalo/persistencia/DAOCompañiaTest.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.uam.ayd.SistemaAbarrotesLalo.persistencia;
+
+import java.util.ArrayList;
+import mx.uam.ayd.SistemaAbarrotesLalo.modelo.Compañia;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Byakuya
+ */
+public class DAOCompañiaTest {
+    
+    public DAOCompañiaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of AgregarCompañia method, of class DAOCompañia.
+     */
+    @Test
+    public void testAgregarCompañia() throws Exception {
+        System.out.println("AgregarCompa\u00f1ia");
+        String nombre = "";
+        DAOCompañia instance = new DAOCompañia();
+        boolean expResult = false;
+        boolean result = instance.AgregarCompañia(nombre);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of recuperarCompañias method, of class DAOCompañia.
+     */
+    @Test
+    public void testRecuperarCompañias() throws Exception {
+        System.out.println("recuperarCompa\u00f1ias");
+        DAOCompañia instance = new DAOCompañia();
+        ArrayList<String> expResult = null;
+        ArrayList<String> result = instance.recuperarCompañias();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of modificarCompañia method, of class DAOCompañia.
+     */
+    @Test
+    public void testModificarCompañia() {
+        System.out.println("modificarCompa\u00f1ia");
+        String id = "";
+        String nombre = "";
+        DAOCompañia instance = new DAOCompañia();
+        Compañia expResult = null;
+        Compañia result = instance.modificarCompañia(id, nombre);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of recuperarCompañia method, of class DAOCompañia.
+     */
+    @Test
+    public void testRecuperarCompañia() {
+        System.out.println("recuperarCompa\u00f1ia");
+        DAOCompañia instance = new DAOCompañia();
+        ArrayList<String> expResult = null;
+        ArrayList<String> result = instance.recuperarCompañia();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //    fail("The test case is a prototype.");
+    }
+    
+}
